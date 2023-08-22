1ad54d05190ced02a1e587475394889015a30f36
bourkison <harrisonbourke97@gmail.com>
2023-08-22 11:21:55 +0100

    Further animations to selectedPub

--- components/Maps/SelectedPub.tsx
+++ components/Maps/SelectedPub.tsx
@@ -1,7 +1,7 @@
 import { distanceString, roundToNearest } from '@/services';
 import { supabase } from '@/services/supabase';
 import { PubSchema } from '@/types';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { selectPub } from '@/store/slices/map';
@@ -9,7 +9,22 @@ import { useAppDispatch } from '@/store/hooks';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { MainNavigatorStackParamList } from '@/nav/MainNavigator';
-import Animated, { FadeInDown, FadeOutDown } from 'react-native-reanimated';
+import Animated, {
+    Easing,
+    EntryExitAnimationFunction,
+    ExitAnimationsValues,
+    Extrapolation,
+    FadeInDown,
+    FadeOutDown,
+    LayoutAnimation,
+    interpolate,
+    runOnJS,
+    useAnimatedStyle,
+    useDerivedValue,
+    useSharedValue,
+    withTiming,
+} from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 type SelectedPubProps = {
     pub: PubSchema;
@@ -25,6 +40,56 @@ export default function SelectedPub({ pub }: SelectedPubProps) {
     const navigation =
         useNavigation<StackNavigationProp<MainNavigatorStackParamList>>();
 
+    const sTranslateY = useSharedValue(0);
+    const sOpacity = useDerivedValue(() =>
+        interpolate(sTranslateY.value, [0, 50], [1, 0.4], {
+            extrapolateLeft: Extrapolation.CLAMP,
+            extrapolateRight: Extrapolation.CLAMP,
+        }),
+    );
+    const contextY = useSharedValue(0);
+
+    const showExitAnimation = useSharedValue(true);
+    const fadeOutDownAnimation =
+        new FadeOutDown().build() as EntryExitAnimationFunction;
+
+    const rStyle = useAnimatedStyle(() => ({
+        transform: [{ translateY: sTranslateY.value }],
+        opacity: sOpacity.value,
+    }));
+
+    const deselectPub = useCallback(() => {
+        dispatch(selectPub(undefined));
+    }, [dispatch]);
+
+    const panGesture = Gesture.Pan()
+        .onStart(() => {
+            contextY.value = sTranslateY.value;
+        })
+        .onUpdate(e => {
+            sTranslateY.value = interpolate(
+                e.translationY + contextY.value,
+                [-200, 0, 100],
+                [-20, 0, 50],
+                {
+                    extrapolateLeft: Extrapolation.CLAMP,
+                    extrapolateRight: Extrapolation.EXTEND,
+                },
+            );
+        })
+        .onFinalize(() => {
+            if (sTranslateY.value > 50) {
+                showExitAnimation.value = false;
+                runOnJS(deselectPub)();
+                return;
+            }
+
+            sTranslateY.value = withTiming(0, {
+                duration: 300,
+                easing: Easing.inOut(Easing.quad),
+            });
+        });
+
     useEffect(() => {
         if (pub.photos[0]) {
             const url = supabase.storage
@@ -32,54 +97,84 @@ export default function SelectedPub({ pub }: SelectedPubProps) {
                 .getPublicUrl(pub.photos[0]);
 
             setImageUrl(url.data.publicUrl);
-
-            console.log('SELECTED PUB:', url);
         } else {
             setImageUrl('');
         }
     }, [pub]);
 
-    const deselectPub = () => {
-        dispatch(selectPub(undefined));
+    const CustomExitingAnimation: EntryExitAnimationFunction = (
+        values: ExitAnimationsValues,
+    ): LayoutAnimation => {
+        'worklet';
+
+        if (showExitAnimation.value === true) {
+            // @ts-ignore
+            return fadeOutDownAnimation(values);
+        }
+
+        const animations = {
+            opacity: withTiming(0, { duration: 150 }),
+        };
+
+        const initialValues = {
+            originY: sTranslateY.value,
+            opacity: sOpacity.value,
+        };
+
+        return { animations, initialValues };
     };
 
     return (
-        <Animated.View entering={FadeInDown} exiting={FadeOutDown}>
-            <Pressable
-                style={styles.container}
-                onPress={() => navigation.navigate('PubView', { pub })}>
-                {imageUrl ? (
-                    <Image source={{ uri: imageUrl }} style={styles.image} />
-                ) : undefined}
+        <Animated.View
+            style={rStyle}
+            entering={FadeInDown}
+            exiting={CustomExitingAnimation}>
+            <GestureDetector gesture={panGesture}>
+                <Pressable
+                    style={styles.container}
+                    onPress={() => navigation.navigate('PubView', { pub })}>
+                    {imageUrl ? (
+                        <Image
+                            source={{ uri: imageUrl }}
+                            style={styles.image}
+                        />
+                    ) : undefined}
 
-                <View style={styles.contentContainer}>
-                    <View>
-                        <Text style={styles.titleText}>{pub.name}</Text>
-                        <Text style={styles.addressText}>{pub.address}</Text>
-                    </View>
-                    <View style={styles.bottomContentRowContainer}>
-                        <Text style={styles.distText}>
-                            {distanceString(pub.dist_meters)}
-                        </Text>
-                        <View style={styles.ratingsContainer}>
-                            <Ionicons name="star" size={10} color="#FFD700" />
-                            <Text style={styles.ratingsText}>
-                                {roundToNearest(
-                                    pub.overall_reviews,
-                                    0.1,
-                                ).toFixed(1)}{' '}
-                                ({pub.num_reviews})
+                    <View style={styles.contentContainer}>
+                        <View>
+                            <Text style={styles.titleText}>{pub.name}</Text>
+                            <Text style={styles.addressText}>
+                                {pub.address}
                             </Text>
                         </View>
+                        <View style={styles.bottomContentRowContainer}>
+                            <Text style={styles.distText}>
+                                {distanceString(pub.dist_meters)}
+                            </Text>
+                            <View style={styles.ratingsContainer}>
+                                <Ionicons
+                                    name="star"
+                                    size={10}
+                                    color="#FFD700"
+                                />
+                                <Text style={styles.ratingsText}>
+                                    {roundToNearest(
+                                        pub.overall_reviews,
+                                        0.1,
+                                    ).toFixed(1)}{' '}
+                                    ({pub.num_reviews})
+                                </Text>
+                            </View>
+                        </View>
                     </View>
-                </View>
 
-                <Pressable
-                    style={styles.closeButtonContainer}
-                    onPress={deselectPub}>
-                    <Text style={styles.closeButtonText}>x</Text>
+                    <Pressable
+                        style={styles.closeButtonContainer}
+                        onPress={deselectPub}>
+                        <Text style={styles.closeButtonText}>x</Text>
+                    </Pressable>
                 </Pressable>
-            </Pressable>
+            </GestureDetector>
         </Animated.View>
     );
 }
