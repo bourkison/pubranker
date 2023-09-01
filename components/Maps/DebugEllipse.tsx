3d92999428e5924aadcac51b74dcedd52dbe9c7f
bourkison <harrisonbourke97@gmail.com>
2023-09-01 01:33:19 +0100

    Merging overlapping ellipses in Debugellipse

--- components/Maps/DebugEllipse.tsx
+++ components/Maps/DebugEllipse.tsx
@@ -33,15 +33,84 @@ export default function DebugEllipse({ pubs, deltas }: DebugEllipseProps) {
     }, [deltas, width, height]);
 
     const ellipseGeoJsonObjects = useMemo(() => {
-        const features = pubs.map(pub => {
-            const location = parseLocation(pub.location);
+        let features: turf.helpers.Feature<
+            turf.helpers.Polygon | turf.helpers.MultiPolygon,
+            turf.helpers.Properties
+        >[] = [];
 
-            const polygon = turf.ellipse(location, radiusWidth, radiusHeight, {
+        if (!radiusWidth || !radiusHeight) {
+            return turf.featureCollection(features);
+        }
+
+        const pubsWithPoints = pubs.map(pub => ({
+            name: pub.name,
+            location: parseLocation(pub.location),
+        }));
+
+        const recursiveCheckCollision = (
+            inputPolygon: turf.helpers.Feature<
+                turf.helpers.MultiPolygon | turf.helpers.Polygon
+            >,
+            startIndex: number,
+        ): [turf.helpers.Point, number] | undefined => {
+            for (let i = startIndex; i < pubsWithPoints.length; i++) {
+                if (
+                    turf.booleanPointInPolygon(
+                        pubsWithPoints[i].location,
+                        inputPolygon,
+                    )
+                ) {
+                    return [pubsWithPoints[i].location, i];
+                }
+            }
+        };
+
+        for (let i = 0; i < pubsWithPoints.length; i++) {
+            const pub = pubsWithPoints[i];
+
+            console.log('ellipse created', pub.name);
+
+            let polygon: turf.helpers.Feature<
+                turf.helpers.MultiPolygon | turf.helpers.Polygon
+            > = turf.ellipse(pub.location, radiusWidth, radiusHeight, {
                 units: 'degrees',
             });
 
-            return polygon;
-        });
+            // Check for collisions (only need to check future ones asprevious ones have already checked us)
+            // As we add in more polygons there may be more collisions, hence we do this recursively.
+            while (true) {
+                const collision = recursiveCheckCollision(polygon, i + 1);
+
+                if (!collision) {
+                    console.log('No collision');
+                    features.push(polygon);
+                    break;
+                }
+
+                const [collisionPoint, collisionPointIndex] = collision;
+
+                console.log(
+                    'collision between',
+                    pub.name,
+                    pubsWithPoints[collisionPointIndex].name,
+                );
+
+                const union = turf.union(
+                    polygon,
+                    turf.ellipse(collisionPoint, radiusWidth, radiusHeight, {
+                        units: 'degrees',
+                    }),
+                );
+
+                if (!union) {
+                    console.warn('error in polygon union');
+                    continue;
+                }
+
+                polygon = union;
+                pubsWithPoints.splice(collisionPointIndex, 1);
+            }
+        }
 
         return turf.featureCollection(features);
     }, [pubs, radiusWidth, radiusHeight]);
