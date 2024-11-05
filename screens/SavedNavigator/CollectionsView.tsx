55a7213b1ae5197c530280a92c010751c725b265
bourkison <harrisonbourke97@gmail.com>
2024-11-05 22:18:03 +0000

    Collection styling

--- screens/SavedNavigator/CollectionsView.tsx
+++ screens/SavedNavigator/CollectionsView.tsx
@@ -2,7 +2,7 @@ import { PubItemType } from '@/components/Pubs/PubItem';
 import SavedListItem from '@/components/Saves/SavedListItem';
 import { SavedNavigatorStackParamList } from '@/nav/SavedNavigator';
 import { supabase } from '@/services/supabase';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome6 } from '@expo/vector-icons';
 import { StackScreenProps } from '@react-navigation/stack';
 import React, { useCallback, useEffect, useState } from 'react';
 import {
@@ -15,11 +15,19 @@ import {
     ActivityIndicator,
 } from 'react-native';
 import * as Location from 'expo-location';
+import UserAvatar from '@/components/User/UserAvatar';
+import { PRIMARY_COLOR } from '@/constants';
 
 type CollectionType = {
     id: number;
     name: string;
+    description: string | null;
     pubs: PubItemType[];
+    user: {
+        id: string;
+        name: string;
+        profile_photo: string | null;
+    };
 };
 
 export default function CollectionsView({
@@ -28,6 +36,7 @@ export default function CollectionsView({
 }: StackScreenProps<SavedNavigatorStackParamList, 'CollectionsView'>) {
     const [isLoading, setIsLoading] = useState(false);
     const [collection, setCollection] = useState<CollectionType>();
+    const [isFollowed, setIsFollowed] = useState(false);
 
     useEffect(() => {
         const fetchCollection = async () => {
@@ -47,7 +56,9 @@ export default function CollectionsView({
                     `
                 id,
                 name,
-                collection_items(pub_id, created_at)
+                description,
+                collection_items(pub_id, created_at),
+                user:users_public(id, name, profile_photo)
                 `,
                 )
                 .eq('id', route.params.collectionId)
@@ -63,6 +74,8 @@ export default function CollectionsView({
                 setIsLoading(false);
                 return;
             }
+
+            console.log('collection', data);
 
             const { coords } = await Location.getCurrentPositionAsync();
 
@@ -103,7 +116,9 @@ export default function CollectionsView({
             setCollection({
                 id: data.id,
                 name: data.name,
+                description: data.description,
                 pubs: orderedPubs,
+                user: data.user,
             });
 
             setIsLoading(false);
@@ -141,9 +156,7 @@ export default function CollectionsView({
                 </TouchableOpacity>
 
                 <View style={styles.headerTextContainer}>
-                    <Text style={styles.headerText}>
-                        {collection?.name || 'Collection'}
-                    </Text>
+                    <Text style={styles.headerText}>Collection</Text>
                 </View>
 
                 <View style={styles.menuContainer} />
@@ -158,6 +171,64 @@ export default function CollectionsView({
                     ) : (
                         <View>
                             <Text>No pubs added to this collection yet</Text>
+                        </View>
+                    )
+                }
+                ListHeaderComponent={
+                    !collection ? undefined : (
+                        <View style={styles.listHeaderContainer}>
+                            <View style={styles.topListHeaderContainer}>
+                                <View style={styles.userContainer}>
+                                    <UserAvatar
+                                        photo={
+                                            collection.user.profile_photo || ''
+                                        }
+                                        size={14}
+                                    />
+
+                                    <Text style={styles.userNameText}>
+                                        {collection.user.name}
+                                    </Text>
+                                </View>
+
+                                <TouchableOpacity
+                                    style={styles.followContainer}
+                                    onPress={() => setIsFollowed(!isFollowed)}>
+                                    {isFollowed ? (
+                                        <>
+                                            <Ionicons
+                                                name="checkmark"
+                                                size={16}
+                                                color={PRIMARY_COLOR}
+                                            />
+                                            <Text style={styles.followText}>
+                                                Followed
+                                            </Text>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <FontAwesome6
+                                                name="plus"
+                                                size={14}
+                                                color={PRIMARY_COLOR}
+                                            />
+                                            <Text style={styles.followText}>
+                                                Follow
+                                            </Text>
+                                        </>
+                                    )}
+                                </TouchableOpacity>
+                            </View>
+
+                            <Text style={styles.collectionNameText}>
+                                {collection.name}
+                            </Text>
+
+                            {collection.description ? (
+                                <Text style={styles.descriptionText}>
+                                    {collection.description}
+                                </Text>
+                            ) : undefined}
                         </View>
                     )
                 }
@@ -205,4 +276,45 @@ const styles = StyleSheet.create({
         paddingLeft: ICON_PADDING,
     },
     menuContainer: {
-        paddingRight: ICON_P
+        paddingRight: ICON_PADDING,
+    },
+    listHeaderContainer: {
+        paddingHorizontal: 15,
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderColor: '#E5E7EB',
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    userNameText: {
+        marginLeft: 5,
+        fontWeight: '300',
+        fontSize: 14,
+    },
+    collectionNameText: {
+        fontSize: 18,
+        fontWeight: '500',
+        fontFamily: 'Jost',
+        marginTop: 10,
+    },
+    descriptionText: {
+        fontSize: 14,
+        marginTop: 20,
+    },
+    topListHeaderContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    followContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    followText: {
+        marginLeft: 4,
+        color: PRIMARY_COLOR,
+        fontWeight: '500',
+    },
+});
