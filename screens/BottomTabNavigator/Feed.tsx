63447ca1d3909ef09304ad0e5666f2307e64d282
bourkison <harrisonbourke97@gmail.com>
2025-02-25 23:36:47 +0000

    Feed filter icon added

--- screens/BottomTabNavigator/Feed.tsx
+++ screens/BottomTabNavigator/Feed.tsx
@@ -2,10 +2,12 @@ import Header from '@/components/Utility/Header';
 import { FeedType } from '@/services/queries/feed';
 import { HomeNavigatorBottomTabProps } from '@/types/nav';
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from 'react-native';
 import FriendsFeed from './FriendsFeed';
 import PageTabs from '@/components/Utility/PageTabs';
 import YouFeed from './YouFeed';
+import { HEADER_ICON_SIZE } from '@/constants';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Feed({}: HomeNavigatorBottomTabProps<'Feed'>) {
     const [selectedPage, setSelectedPage] = useState(0);
@@ -18,7 +20,15 @@ export default function Feed({}: HomeNavigatorBottomTabProps<'Feed'>) {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Header header="Feed" />
+            <Header
+                header="Feed"
+                leftColumn={<View style={styles.emptyIcon} />}
+                rightColumn={
+                    <TouchableOpacity style={styles.optionsContainer}>
+                        <Ionicons name="options" size={HEADER_ICON_SIZE} />
+                    </TouchableOpacity>
+                }
+            />
 
             <View style={styles.pagesContainer}>
                 <PageTabs
@@ -54,6 +64,8 @@ export default function Feed({}: HomeNavigatorBottomTabProps<'Feed'>) {
     );
 }
 
+const ICON_PADDING = 10;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -61,5 +73,14 @@ const styles = StyleSheet.create({
     pagesContainer: {
         flex: 1,
         width: '100%',
+    },
+    emptyIcon: {
+        flex: 1,
+        paddingLeft: ICON_PADDING,
+    },
+    optionsContainer: {
+        flex: 1,
+        paddingRight: ICON_PADDING,
+        alignItems: 'flex-end',
     },
 });
